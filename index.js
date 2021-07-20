96b9fecc82b7313e99caedb3de78683b00c60b90
Samantha Laine <samanthalaine8@gmail.com>
2021-07-20 12:04:38 -0400

    commit tree a67f754bf71021e41d02e74f7f82466e2c9a3277

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    return callback();
+}
+
+function returnsANamedFunction(){
+    return function name(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
